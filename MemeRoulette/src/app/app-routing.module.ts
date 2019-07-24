e41fcdda576a1802bc742112d35ad62953a86514
00587553 <john.cerreta@aa.com>
2019-07-23 19:08:43 -0500

    Added in roulette component

--- MemeRoulette/src/app/app-routing.module.ts
+++ MemeRoulette/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RouletteComponent } from './roulette/roulette.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  redirectTo: '/roulette',
+  pathMatch: 'full'
+},
+{
+  path: 'roulette',
+  component: RouletteComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
